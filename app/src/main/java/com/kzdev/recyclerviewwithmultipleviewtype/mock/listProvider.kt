f9830b71e1899c5863b7111d220de84942d55268
V1n1c1us-K-S-W4t4n4b3 <vinikswatanabe@outlook.com>
2024-03-21 14:32:58 -0400

    last commit

--- app/src/main/java/com/kzdev/recyclerviewwithmultipleviewtype/mock/listProvider.kt
+++ app/src/main/java/com/kzdev/recyclerviewwithmultipleviewtype/mock/listProvider.kt
@@ -1,4 +1,36 @@
 package com.kzdev.recyclerviewwithmultipleviewtype.mock
 
-class list {
+import com.kzdev.recyclerviewwithmultipleviewtype.R
+
+fun getItems(): MutableList<Any> {
+    return mutableListOf(
+        "Hello guys! My name is Muhammad Bilal, I am an android developer with 4 years of experience.",
+        "Hello guys! My name is Muhammad Bilal, I am an android developer with 4 years of experience.",
+        "Hello guys! My name is Muhammad Bilal, I am an android developer with 4 years of experience.",
+        R.drawable._122255,
+        "Hello guys! My name is Muhammad Bilal, I am an android developer with 4 years of experience.",
+        "Hello guys! My name is Muhammad Bilal, I am an android developer with 4 years of experience.",
+        R.drawable._09015,
+        "Hello guys! My name is Muhammad Bilal, I am an android developer with 4 years of experience.",
+        "Hello guys! My name is Muhammad Bilal, I am an android developer with 4 years of experience.",
+        "Hello guys! My name is Muhammad Bilal, I am an android developer with 4 years of experience.",
+        R.drawable.android_hex_wallpaper_1920x1080,
+        "Hello guys! My name is Muhammad Bilal, I am an android developer with 4 years of experience.",
+        "Hello guys! My name is Muhammad Bilal, I am an android developer with 4 years of experience.",
+        R.drawable.canon_85mm_1_2l_ii_wallpaper_2560x1440,
+        "Hello guys! My name is Muhammad Bilal, I am an android developer with 4 years of experience.",
+        "Hello guys! My name is Muhammad Bilal, I am an android developer with 4 years of experience.",
+        "Hello guys! My name is Muhammad Bilal, I am an android developer with 4 years of experience.",
+        R.drawable.cyberpunk_2077_samurai_5k_2j,
+        "Hello guys! My name is Muhammad Bilal, I am an android developer with 4 years of experience.",
+        "Hello guys! My name is Muhammad Bilal, I am an android developer with 4 years of experience.",
+        R.drawable.desktop_wallpaper_zaraki_kenpachi_bankai_bleach_kenpachi,
+        "Hello guys! My name is Muhammad Bilal, I am an android developer with 4 years of experience.",
+        "Hello guys! My name is Muhammad Bilal, I am an android developer with 4 years of experience.",
+        "Hello guys! My name is Muhammad Bilal, I am an android developer with 4 years of experience.",
+        R.drawable.eouqrhbvqaq5dkt,
+        "Hello guys! My name is Muhammad Bilal, I am an android developer with 4 years of experience.",
+        "Hello guys! My name is Muhammad Bilal, I am an android developer with 4 years of experience.",
+        R.drawable.dragonmancer_sett_league_of_legends_8k_9w,
+    )
 }
--- app/src/main/java/com/kzdev/recyclerviewwithmultipleviewtype/ui/activity/MainActivity.kt
+++ app/src/main/java/com/kzdev/recyclerviewwithmultipleviewtype/ui/activity/MainActivity.kt
@@ -1,8 +1,10 @@
-package com.kzdev.recyclerviewwithmultipleviewtype
+package com.kzdev.recyclerviewwithmultipleviewtype.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.kzdev.recyclerviewwithmultipleviewtype.adapter.MultiViewAdapter
 import com.kzdev.recyclerviewwithmultipleviewtype.databinding.ActivityMainBinding
+import com.kzdev.recyclerviewwithmultipleviewtype.mock.getItems
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +17,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val adapter = MultiViewAdapter(this, getItems())
+        binding.recyclerView.adapter = adapter
     }
 }
